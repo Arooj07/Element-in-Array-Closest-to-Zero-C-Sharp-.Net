bd61c4a2d1348833d45053c28c616f9e866f2851
Arooj07 <100277795+Arooj07@users.noreply.github.com>
2023-03-24 23:21:55 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {5,-1,2,-3,4};
+
+            Array.Sort(arr);
+            
+            int y = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] >= 0)
+                {
+                    y = arr[i];
+
+                    Console.WriteLine("Closest to Zero is: " + y);
+                    break;
+
+                }
+            }
+
+        }
+
+    }
+}
